6f855af72b4b6bee7e3432a6ce78995454670df9
apupier <apupier@redhat.com>
2021-11-22 14:19:14 +0000

    modified fork

--- src/main/java/org/acme/sample/Routes.java
+++ src/main/java/org/acme/sample/Routes.java
@@ -7,7 +7,7 @@ public class Routes extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("netty-http:http://0.0.0.0:8080")
-            .transform().constant("Hello from Fuse F2F!");
+            .transform().constant("Hello from my forked Fuse F2F!");
     }
 
 }
